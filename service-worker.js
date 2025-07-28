8c41306d74e2af772b5d36739e6b9aa3da91ab51
Aboubacar DIALLO <gates2012x@gmail.com>
2025-07-28 17:17:05 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = 'gps-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
